f1af77dfa911b72cd03743b0df70271a2696577c
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-16 01:12:58 -0800

    third function added

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function myName(x){
 }
 console.log(myName("some name"))
 
+function circleCircumference(x){
+    return Math.PI * x
+}
+console.log(circleCircumference(6))
+
+function welcome(and) {
+    console.log("Welcome" + " " + and);
+}
+function the(chaos){
+    console.log("the" + " " + chaos)
+}
+console.log(welcome("to") + the("chaos"))
